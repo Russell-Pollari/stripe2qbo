82ed300bb90d4506a0381270046b87c124769480
russell-pollari <pollarir@mgail.com>
2023-08-30 20:16:03 -0400

    Handle errors when fetching stripe transactions

--- client/components/ImportTransactions.tsx
+++ client/components/ImportTransactions.tsx
@@ -7,23 +7,35 @@ import { addTransaction } from '../store/transactions';
 import type { RootState } from '../store/store';
 import type { SyncOptions, Transaction } from '../types';
 import SubmitButton from '../SubmitButton';
+import { useGetStripeInfoQuery } from '../services/api';
 
 const ImportTransactions = () => {
     const dispatch = useDispatch();
     const isSyncing = useSelector((state: RootState) => state.sync.isSyncing);
     const status = useSelector((state: RootState) => state.sync.status);
+    const { data: stripeInfo } = useGetStripeInfoQuery('');
 
     const importTransactions = async (options: SyncOptions) => {
-        const queryString = new URLSearchParams(options).toString();
+        if (!stripeInfo) {
+            alert('No Stripe account connected.');
+            return;
+        }
+
         dispatch(setIsSyncing(true));
         dispatch(setSyncStatus('Importing transactions...'));
+
+        const queryString = new URLSearchParams(options).toString();
         const response = await fetch('/stripe/transactions?' + queryString);
-        const data: Transaction[] = await response.json();
-        if (data) {
+
+        if (response.status === 200) {
+            const data: Transaction[] = await response.json();
             data.forEach((transaction: Transaction) => {
                 dispatch(addTransaction(transaction));
             });
+        } else {
+            alert('Error importing transactions.');
         }
+
         dispatch(setSyncStatus(''));
         dispatch(setIsSyncing(false));
     };
