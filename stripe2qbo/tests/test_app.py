f265fae4054f6a3d6eaefacde808293fc1184909
daniel <danieladedeji997@yahoo.com>
2023-09-05 16:06:34 -0400

    Moved tests folder

--- stripe2qbo/tests/test_app.py
+++ stripe2qbo/tests/test_app.py
@@ -10,24 +10,28 @@ def test_index():
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
 
+
 def test_user_id_unauthorized():
     response = client.get("/userId")
     assert response.status_code == 401
     assert response.text is not None
 
+
 def test_logout():
     response = client.post("/logout")
     assert response.status_code == 200
+
 
 def test_get_settings_unauthorized():
     response = client.get("/settings")
     assert response.status_code == 401
 
+
 def test_save_settings_unauthorized():
     response = client.post("/settings")
     assert response.status_code == 401
 
+
 def test_sync_single_transaction_unauthorized():
     response = client.post("/sync")
     assert response.status_code == 401
-
