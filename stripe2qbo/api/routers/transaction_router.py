e0bba0d0460fe3ce51cd46b06ce2e7dbe950c3fe
daniel <danieladedeji997@yahoo.com>
2023-09-08 13:56:57 -0400

    Addressed PR comments

--- stripe2qbo/api/routers/transaction_router.py
+++ stripe2qbo/api/routers/transaction_router.py
@@ -1,9 +1,8 @@
 from typing import Annotated
 
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 
-from stripe2qbo.api.dependencies import get_current_user, get_db
+from stripe2qbo.api.dependencies import get_current_user
 from stripe2qbo.db.models import TransactionSync, User
 
 router = APIRouter(
@@ -14,30 +13,18 @@ router = APIRouter(
 
 @router.get("/")
 async def get_all_transactions(
-    db: Annotated[Session, Depends(get_db)],
     user: Annotated[User, Depends(get_current_user)],
 ) -> TransactionSync:
-    try:
-        return (
-            db.query(TransactionSync).filter(TransactionSync.user_id == user.id).all()
-        )
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error making request: {e}")
+    return TransactionSync.query.filter(TransactionSync.user_id == user.id).all()
 
 
-@router.get("/{Transaction_id}")
+@router.get("/{transaction_id}")
 async def get_transaction_by_id(
-    db: Annotated[Session, Depends(get_db)],
     user: Annotated[User, Depends(get_current_user)],
-    Transaction_id: str,
+    transaction_id: str,
 ) -> TransactionSync:
-    try:
-        return (
-            db.query(TransactionSync)
-            .filter(
-                TransactionSync.user_id == user.id, TransactionSync.id == Transaction_id
-            )
-            .first()
-        )
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error making request: {e}")
+    return (
+        TransactionSync.query.filter(TransactionSync.user_id == user.id)
+        .filter(TransactionSync.id == transaction_id)
+        .first()
+    )
