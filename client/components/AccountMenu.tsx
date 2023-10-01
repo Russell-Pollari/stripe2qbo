931d892bc04d7b4d3f93338c82aabd371610b8fa
russell-pollari <pollarir@mgail.com>
2023-10-01 11:18:19 -0400

    Fixup logout

--- client/components/AccountMenu.tsx
+++ client/components/AccountMenu.tsx
@@ -7,12 +7,12 @@ import {
 
 import type { QBOCompanyInfo } from '../types';
 
-const AccountMenu = ({ companyInfo }: { companyInfo: QBOCompanyInfo }) => {
-    const logout = () => {
-        localStorage.removeItem('token');
-        location.reload();
-    };
+const logout = () => {
+    document.cookie = 'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    location.reload();
+};
 
+const AccountMenu = ({ companyInfo }: { companyInfo: QBOCompanyInfo }) => {
     return (
         <Menu as="div" className="relative text-left z-50">
             <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
